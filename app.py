fb1217588e6b483cf0c6d9a8eb2668c76a714470
Nrakeshreddy <rakeshreddy9920@gmail.com>
2025-07-05 00:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ from functools import wraps
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  
 # MySQL Configuration
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''  
-app.config['MYSQL_DB'] = 'house_price_db'
+import os
+
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 40167))
+
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
